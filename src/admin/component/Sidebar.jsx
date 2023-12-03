7ed3387e83775d44ac8dc74da2133ec451dea987
dineshkumar97 <dineshkumarppn07@gmail.com>
2023-12-03 17:29:57 +0530

    test

--- src/admin/component/Sidebar.jsx
+++ src/admin/component/Sidebar.jsx
@@ -1,107 +1,101 @@
-import React from 'react';
+import React from "react";
 import styles from "../admin.module.scss";
-
+import logoIcon from "../../assets/Icon.png";
+import { useState } from "react";
 const Sidebar = () => {
+    // toggleClass = () => {
+    //     const oldClassName = document.getElementById('test').className;
+    //     const newClassName = oldClassName === 'red' ? 'blue' : 'red'
+    //     document.getElementById('test').className = newClassName
+    //     console.log('tttt')
+    //   }
+    const [isActive, setActive] = useState(false);
+  return (
+    <div>
+      {/* <section id={`${styles.sidebar}${isActive ? styles.hide : ""}`}> */}
+      <section id={`${styles.sidebar}`}>
+        <p className={`${styles.brand}`}>
+          <i className={`${styles.bx_icon} bx bx-smile`}></i>
+          <span className={`${styles.text}`}  >AdminHub</span>
+        </p>
+        <ul className={`${styles.side_menu} ${styles.top}`}>
+          <li className={`${styles.active}`}>
+            <p>
+              <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+              <span className={`${styles.text}`}>Dashboard</span>
+            </p>
+          </li>
+          <li>
+            <p>
+              <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+              <span className={`${styles.text}`}>Dashboard</span>
+            </p>
+          </li>
+          <li>
+            <p>
+              <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+              <span className={`${styles.text}`}>Dashboard</span>
+            </p>
+          </li>
+          <li>
+            <p>
+              <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+              <span className={`${styles.text}`}>Dashboard</span>
+            </p>
+          </li>
+        </ul>
+        <ul className={`${styles.side_menu}`} >
+          <li>
+            <p>
+              <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+              <span className={`${styles.text}`}>Setting</span>
+            </p>
+          </li>
+          <li>
+            <p>
+              <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+              <span className={`${styles.text}`}>Dashboard</span>
+            </p>
+          </li>
+          <li>
+            <p>
+              <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+              <span className={`${styles.text}`}>Dashboard</span>
+            </p>
+          </li>
+          <li>
+            <p>
+              <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+              <span className={`${styles.text}`}>Dashboard</span>
+            </p>
+          </li>
+        </ul>
+      </section>
 
-    const allSideMenu = document.querySelectorAll('#sidebar');
-    allSideMenu.forEach(item => {
-        const li = item.parentElement;
-        item.addEventListener('click', function () {
-            allSideMenu.forEach(i => {
-                console.log("test")
-                i.parentElement.classList.remove('active');
-            })
-            li.classList.add('active');
-        })
-    });
- 
+      <section id={`${styles.content}`}>
+        <nav>
+          <i className={`${styles.cursor} bx bx-menu`} onClick={() => setActive(!isActive)}></i>
+          <a className={`${styles.nav_link} `}> Category</a>
+          <form>
+            <div className={`${styles.form_input} bx bxs-menu`}>
+              <input type="search" />
+              <button type="submit">
+                <i className={` bx bx-search`}></i>
+              </button>
+            </div>
+          </form>
 
-    return (
-        <div>
-            <section id={`${styles.sidebar}`} >
-                <p className={`${styles.brand}`}>
-                    <i className={`${styles.bx_icon} bx bx-smile`}></i>
-                    <span className={`${styles.text}`}>AdminHub</span>
-                </p>
-                <ul className={`${styles.side_menu} ${styles.top}`}>
-                    <li className={`${styles.active}`}>
-                        <p>
-                            <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
-                            <span className={`${styles.text}`}>Dashboard</span>
-                        </p>
-                    </li>
-                    <li >
-                        <p>
-                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
-                            <span className={`${styles.text}`}>Dashboard</span>
-                        </p>
-                    </li>
-                    <li>
-                        <p>
-                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
-                            <span className={`${styles.text}`}>Dashboard</span>
-                        </p>
-                    </li>
-                    <li>
-                        <p>
-                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
-                            <span className={`${styles.text}`}>Dashboard</span>
-                        </p>
-                    </li>
-
-                </ul>
-                <ul className={`${styles.side_menu}`}>
-                    <li>
-                        <p>
-                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
-                            <span className={`${styles.text}`}>Setting</span>
-                        </p>
-                    </li>
-                    <li>
-                        <p>
-                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
-                            <span className={`${styles.text}`}>Dashboard</span>
-                        </p>
-                    </li>
-                    <li>
-                        <p>
-                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
-                            <span className={`${styles.text}`}>Dashboard</span>
-                        </p>
-                    </li>
-                    <li>
-                        <p>
-                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
-                            <span className={`${styles.text}`}>Dashboard</span>
-                        </p>
-                    </li>
-
-                </ul>
-            </section>
-            
-            <section id={`${styles.content}`} >
-                <nav>
-                    <i  className={`${styles.cursor} bx bx-menu`}></i>
-                    <a > Category</a>
-                    <form>
-                        <div class={`${styles.form_input} bx bxs-menu`}>
-                            <input type='search' />
-                            <button type='submit'>
-                                <i className={` bx bx-search`}></i>
-                            </button>
-                        </div>
-                    </form>
-
-                    <a  className={`${styles.notification}`}>
-                    <i className={`${styles.bx_icon} bx bxs-bell`}></i>
-                    <span>8</span>
-                    </a>
-                </nav>
-
-            </section>
-        </div>
-
-    )
+          <a className={`${styles.notification}`}>
+            <i className={`${styles.bx_icon} bx bxs-bell`}></i>
+            <span className={`${styles.num}`}>8</span>
+          </a>
+          <a>
+            <img  src={logoIcon}  className={`${styles.profileimage}`}  alt="Logo Icon"/>
+          </a>
+        </nav>
+      </section>
+    </div>
+  );
 };
 
 export default Sidebar;
---
+++ src/admin/component/script.js
@@ -0,0 +1,7 @@
+const menubar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+
+menubar.addEventListener('click',function(){
+    sidebar.classList.toggle('hide');
+    console.log('className')
+})
