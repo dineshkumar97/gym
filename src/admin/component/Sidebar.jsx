f6aa483817311402cfd4d1b3d5c6be2a5dd88332
dineshkumar97 <dineshkumarppn07@gmail.com>
2023-12-03 11:37:09 +0530

    test

--- src/admin/component/Sidebar.jsx
+++ src/admin/component/Sidebar.jsx
@@ -1,37 +1,107 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import styles from "../admin.module.scss";
-import Employee from "./Employee";
-import Footer from "./Footer";
-import { Link } from "react-router-dom";
-import Header from "./Header";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-const Sidebar = () => {
-    return (
 
-        <div className={`${styles.parent}`}>
-            <Header />
-            <div className={`${styles.main}`}>
-                <div className={`${styles.content_area}`}>
-                    <div className={`${styles.sidebar_area}`} role="complementary">
-                        <nav className={`${styles.side_navigation}`} role="navigation">
-                            <ul className={`${styles.menu}`}>
-                                <li className={`${styles.menu_item}`}>
-                                    <Link to="/Employee" className="iconContainer">Employee</Link>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-                <Suspense >
-                    <Routes>
-                        <Route path={"/"} element={<Employee />} />
-                        <Route path={"/test"} element={<Employee />} />
-                    </Routes>
-                </Suspense>
-            </div>
-            {/* <Footer /> */}
+const Sidebar = () => {
+
+    const allSideMenu = document.querySelectorAll('#sidebar');
+    allSideMenu.forEach(item => {
+        const li = item.parentElement;
+        item.addEventListener('click', function () {
+            allSideMenu.forEach(i => {
+                console.log("test")
+                i.parentElement.classList.remove('active');
+            })
+            li.classList.add('active');
+        })
+    });
+ 
+
+    return (
+        <div>
+            <section id={`${styles.sidebar}`} >
+                <p className={`${styles.brand}`}>
+                    <i className={`${styles.bx_icon} bx bx-smile`}></i>
+                    <span className={`${styles.text}`}>AdminHub</span>
+                </p>
+                <ul className={`${styles.side_menu} ${styles.top}`}>
+                    <li className={`${styles.active}`}>
+                        <p>
+                            <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+                            <span className={`${styles.text}`}>Dashboard</span>
+                        </p>
+                    </li>
+                    <li >
+                        <p>
+                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+                            <span className={`${styles.text}`}>Dashboard</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+                            <span className={`${styles.text}`}>Dashboard</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+                            <span className={`${styles.text}`}>Dashboard</span>
+                        </p>
+                    </li>
+
+                </ul>
+                <ul className={`${styles.side_menu}`}>
+                    <li>
+                        <p>
+                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+                            <span className={`${styles.text}`}>Setting</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+                            <span className={`${styles.text}`}>Dashboard</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+                            <span className={`${styles.text}`}>Dashboard</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                             <i className={`${styles.bx_icon} bx bxs-dashboard`}></i>
+                            <span className={`${styles.text}`}>Dashboard</span>
+                        </p>
+                    </li>
+
+                </ul>
+            </section>
+            
+            <section id={`${styles.content}`} >
+                <nav>
+                    <i  className={`${styles.cursor} bx bx-menu`}></i>
+                    <a > Category</a>
+                    <form>
+                        <div class={`${styles.form_input} bx bxs-menu`}>
+                            <input type='search' />
+                            <button type='submit'>
+                                <i className={` bx bx-search`}></i>
+                            </button>
+                        </div>
+                    </form>
+
+                    <a  className={`${styles.notification}`}>
+                    <i className={`${styles.bx_icon} bx bxs-bell`}></i>
+                    <span>8</span>
+                    </a>
+                </nav>
+
+            </section>
         </div>
-    );
+
+    )
 };
 
 export default Sidebar;
